65d372eeeb320510dfbcca0b82c9445689e80f7e
Dmitri Krasutski <krasutski.dmitri@gmail.com>
2018-11-08 13:28:05 +0300

    dwdwd

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -82,7 +82,7 @@ alert('Username: ' + this.username.value + " Password: " + this.password.value +
            <div className="container">
                <div className="row row-header">
                    <div className="col-12 col-sm-6">
-                       <h1>Ristorante con Fusion</h1>
+                       <h1>Ristorante Fusion</h1>
                        <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                    </div>
                </div>
